ca03a9aef0e1815b7e260752f9b7a8441685e545
Beau Scott <beau.scott@gmail.com>
2015-10-18 03:18:21 -0600

    Initial check in

---
+++ server.js
@@ -0,0 +1,10 @@
+    var express = require('express');
+    var app = express();
+    app.use(express.static('www')); //Server-static HTML, JS, CSS, etc, goes here.
+    // HTTP GET operation to get a JSON result. You could POST/PUT/DELETE/etc., too.
+    app.get('/api/someJsonEndpoint', function (request, result) {
+        console.log('api hit');
+        result.json({result:"Hello World"}); // return json encoded object
+    });
+    // ... create new endpoints for each object, or make it dynamic
+    app.listen(8080);
