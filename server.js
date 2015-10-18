27017819d0e336a60a87fc15a7ba56cad820a196
Beau Scott <beau.scott@gmail.com>
2015-10-18 04:00:28 -0600

    Added better code comments

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 "use strict";
 var express = require('express'),
     app = express();
-app.use(express.static('www')); //Server-static HTML, JS, CSS, etc, goes here.
-// HTTP GET operation to get a JSON result. You could POST/PUT/DELETE/etc., too.
-app.get('/api/someJsonEndpoint', function (request, result) {
-    console.log('api hit');
-    result.json({result: "Hello World"}); // return json encoded object
+
+// Tell express to serve all files under the www directory statically, if they exist.
+// These will be served from the root of the url, so http://localhost:8080/index.html will map to www/index.html
+// HTML, CSS, client-side JS, images... any static content belongs under this directory.
+app.use(express.static('www'));
+
+// Tell express that GET requests matching the url of /api/someJsonEndpoint get served with this function
+// Note: there are other operations supported here, like post, put, delete
+app.get('/api/someJsonEndpoint', function (request, response) {
+    response.json({result: "Hello World"}); // return json encoded object
 });
-// ... create new endpoints for each object, or make it dynamic
+
+// Start the webserver
 app.listen(8080);
